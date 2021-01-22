0a015f2bb93c3c9c4e97ab30d120edf85ee7b51b
ar1st <aristnm@hotmail.com>
2021-01-22 16:17:27 +0200

    style added in login and main

--- src/main/kotlin/com/aris/booklibraries/demoBookLibraries/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/aris/booklibraries/demoBookLibraries/config/WebSecurityConfig.kt
@@ -4,15 +4,15 @@ import com.aris.booklibraries.demoBookLibraries.models.Role
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import javax.sql.DataSource
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+
 
 @Configuration
 @EnableWebSecurity
@@ -38,7 +38,7 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
         http
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/admin/**").hasRole( Role.ADMIN.value )
+            .antMatchers("/admin/**").hasRole(Role.ADMIN.value)
             .antMatchers("/").permitAll()
             .antMatchers("/login*").permitAll()
             .antMatchers("/sign*").permitAll()
@@ -58,6 +58,13 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     @Bean
     fun passwordEncoder(): PasswordEncoder? {
         return BCryptPasswordEncoder()
+    }
+
+    @Throws(java.lang.Exception::class)
+    override fun configure(web: WebSecurity) {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/icon/**")
     }
 
 }
--- src/main/resources/static/js/actions.js
+++
@@ -1,8 +0,0 @@
-function demo(){
-    alert("Thymeleaf with CSS and JS demo.")
-}
-
-function fetchBooks(libraryId) {
-    alert( libraryId )
-
-}
