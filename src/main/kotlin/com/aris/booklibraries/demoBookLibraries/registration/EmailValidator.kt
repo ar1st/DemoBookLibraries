428522e602a6d41a1973e2a464fd80452e9bc7df
ar1st <aristnm@hotmail.com>
2021-02-03 10:24:23 +0200

    checks for valid email

--- src/main/kotlin/com/aris/booklibraries/demoBookLibraries/registration/EmailValidator.kt
+++ src/main/kotlin/com/aris/booklibraries/demoBookLibraries/registration/EmailValidator.kt
@@ -5,8 +5,9 @@ import java.util.function.Predicate
 
 @Service
 class EmailValidator: Predicate<String> {
-    override fun test(t: String): Boolean {
+    private val EMAIL_PATTERN = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$"
+    override fun test(email: String): Boolean {
         //todo implement this.
-        return true;
+        return email.matches(EMAIL_PATTERN.toRegex());
     }
 }
